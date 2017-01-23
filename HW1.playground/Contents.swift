f3efda5a92d3dc1d4c1f7e79efcba6ba8c1ddb8e
Nam Nguyen <namnguyen1995@gmail.com>
2017-01-23 16:20:07 -0500

    Problems 1-4

---
+++ HW1.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+//Problem 1
+let myString = "hello"
+var cost: Double = 3.14
+let cnt: Int = 2
+var shouldWe: Bool = true
+let intConst = 17
+let intConst2 = 00001010
+
+//Problem 2
+var num: Float = 1.23456
+var text: String = "The number is"
+var finalText: String = "\(text) " + "\(num*num)"
+
+//Problem 3
+var arr = ["queen", "worker", "drone"]
+print(arr[0])
+arr.append("honey")
+arr += ["are", "us"]
+
+//Problem 4
