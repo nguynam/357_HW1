5195a907797e2c656625401ef122d7d7ecd2e918
Nam Nguyen <namnguyen1995@gmail.com>
2017-01-24 16:36:27 -0500

    Problems 1-10

--- HW1.playground/Contents.swift
+++ HW1.playground/Contents.swift
@@ -28,4 +28,44 @@ for(index, value) in arr.enumerated() {
 }
 
 //Problem 5
+var names: Dictionary<String,Float> = ["Mark Twait":8.9, "Nathaniel Hawthorne":5.1, "John Steinbeck":2.3, "C.S. Lewis":9.9, "John Krakaur":6.1]
+
+//Problem 6
+var value = names["John Steinbeck"]
+print(value!)
+names["Erik Larson"] = 9.2
+var John = names["John Krakaur"]
+var Mark = names["Mark Twait"]
+if(John! > Mark!){
+    print("John Krakaur Wins")
+}
+else{
+    print("Mark Twait Wins")
+}
+
+//Problem 7
+for (key, value) in names {
+    print("\(key) : \(value)")
+}
+
+//Problem 8
+for index in 1...10 {
+    print("\(index)")
+}
+
+//Problem 9
+for index in (1...10).reversed() {
+    print("\(index)")
+}
+
+//Problem 10
+var x = 5
+var y = 5
+var product = 0
+for _ in 1...y {
+    product += x
+}
+print(product)
+
+//Problem 11
 
